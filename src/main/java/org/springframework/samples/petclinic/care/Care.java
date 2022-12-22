f5a505ced740c0ea304483813a08a8dd422fe297
Ignacio Gonzalez <igngongon2@alum.us.es>
2022-12-22 11:59:20 +0100

    Sol1

--- src/main/java/org/springframework/samples/petclinic/care/Care.java
+++ src/main/java/org/springframework/samples/petclinic/care/Care.java
@@ -2,6 +2,18 @@ package org.springframework.samples.petclinic.care;
 
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -9,8 +21,26 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Care {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    
+    @Column(unique = true)
+    @NotNull
+    @NotEmpty
+    @Min(3)
+    @Max(40)
     String name;
+
+    @Min(1)
+    @Max(120)
+    @Column(name = "care_duration")
     int careDuration;
+
+    @NotEmpty
+    @NotNull
+    @ManyToMany(cascade = CascadeType.ALL)
     Set<PetType> compatiblePetTypes;
 }
